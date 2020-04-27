0b6b991aa0859a0ff665ec50282242cc48d94f64
Kim Steinhaug <kim@steinhaug.com>
2020-04-27 12:27:25 +0200

    Added curl fetch of asset. Create asset locally.

--- public_html/myownphpcdn.functions.php
+++ public_html/myownphpcdn.functions.php
@@ -13,7 +13,7 @@
  * @package myownphpcdn
  */
 
-define('mopcdn_debug_level', 1);
+define('mopcdn_debug_level', 0);
 
 /**
  * Removes trailing slash if it exists.
@@ -40,7 +40,7 @@ function make_folders($paths_string, $root = null){
 
     // If root is not given default setup is defined
     if( is_null($root) OR empty($root) )
-        $root = __dir__;
+        $root = __DIR__;
 
     $paths_string = untrailingslashit($paths_string);
     $paths = explode('/', $paths_string);
@@ -55,7 +55,7 @@ function make_folders($paths_string, $root = null){
             continue;
 
         $dirpath = $dirpath . '/' . $dir;
-        if( mopcdn_debug_level ) echo $dirpath . '<br>';
+        if( mopcdn_debug_level ) echo 'mkdir ' . $dirpath . '<br>';
 
         if(!file_exists($dirpath)){
             if ( ! @mkdir($dirpath) ){
@@ -78,4 +78,97 @@ function trigger_webmaster_alert($debug_backtrace){
     // Function does not do anything at the moment
 
     return null;
+}
+
+function request($url,$headers=array(),$params=array(),$referrer="",$http_code=false){
+    //global $cachemode;
+
+        static $cookiefile = "cpanel_cookie.txt";
+        static $curlfile = "cpanel_curl.txt";
+
+        if(!empty($headers)){
+            $headers = [
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                "accept-encoding: deflate, br",
+                "Accept-Language: nb-NO,nb;q=0.9,en;q=0.8,no;q=0.7,en-US;q=0.6,sv;q=0.5,da;q=0.4",
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safar"
+            ];
+        }
+    
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        //curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0');
+        if(!empty($headers)){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        if(!empty($params)){
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, realpath($cookiefile));
+        curl_setopt($ch, CURLOPT_COOKIEFILE, realpath($cookiefile));
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        if(!empty($referrer)){
+           curl_setopt($ch, CURLOPT_REFERER, $referrer);  
+        }
+        $answer = curl_exec($ch);
+
+        //if($http_code){
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+            $status = curl_getinfo($ch);
+        //}
+        curl_close($ch);
+
+        if($http_code){
+            if($status['http_code'] == 302){
+              $answer = $status['redirect_url'];
+            }
+        }
+
+        //echo '<pre>';
+        //var_dump($status);
+        return [$answer, $status];
+    }
+
+function logfile($the_string, $logfile='errors', $lf="\n" ){
+    if( !is_string($the_string) ){
+        $the_string = json_encode($the_string, true);
+        $the_string = str_replace(',"',',' . "\n" . ' "', $the_string);
+    }
+
+    if( $fh = @fopen( dirname(__FILE__) . '/.' . $logfile . ".log", "a+" ) ){
+        fputs( $fh, $the_string . $lf, strlen($the_string . $lf) );
+        fclose( $fh );
+        return( true );
+    } else {
+        return( false );
+    }
+}
+
+/**
+ * Make sure string ends with a /
+ */
+function with_ending_seperator($path){
+    if(empty($path))
+        return '/';
+    if( preg_match("/\/$/", $path) )
+        return $path;
+        else
+        return $path . '/';
+}
+
+/**
+ * Make sure string starts without / but ends with /
+ */
+function without_starting_seperator($path){
+    $path = with_ending_seperator($path);
+    if($path == '/') return '';
+    if( preg_match("/^\//", $path) )
+        return substr($path,1);
+        else
+        return $path;
 }
--- public_html/myownphpcdn.php
+++ public_html/myownphpcdn.php
@@ -11,6 +11,7 @@
  */
 
 define('ASSETS_DOMAIN', 'http://mysite.com');
+define('ASSETS_PATH',   '/vedlegg'); // '' or subdir as '/somedir'
 define('ASSETS_RETRIEVAL', 'file'); // file, curl
 define('ASSETS_REPOSITORY_PATH', '../public_html');
 define('FS_CHMOD_DIR', ( 0755 & ~ umask() ));
@@ -21,3 +22,39 @@ require __dir__ . '/myownphpcdn.functions.php';
 // locate and retrieve asset
 // optimize asset and save it
 // deliver asset
+
+$serverpath = dirname(__FILE__);
+$asset = pathinfo($_GET['url']);
+
+if( empty($_GET['url']) ){
+    http_response_code(404);
+    exit;
+}
+
+if( empty($asset['dirname']) ){
+    logfile($_GET);
+    http_response_code(500);
+    exit;
+}
+
+$remote_url = ASSETS_DOMAIN . ASSETS_PATH . with_ending_seperator($asset['dirname']) . $asset['basename'];
+$local_url = dirname(__FILE__) . with_ending_seperator($asset['dirname']) . $asset['basename'];
+
+list($the_asset,$status) = request($remote_url);
+if($status['http_code'] == 200){
+    if( $asset['dirname'] != '/' ){
+        make_folders($asset['dirname']);
+    }
+    file_put_contents($local_url, $the_asset);
+    if( file_exists($local_url) )
+        header('Location: ' . without_starting_seperator($asset['dirname']) . $asset['basename']);
+        else
+        http_response_code(404);
+} else if($status['http_code'] == 404){
+    http_response_code(404);
+    exit;
+} else {
+    logfile($status);
+}
+
+http_response_code(404);
